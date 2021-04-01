72dfcf91e6670361ecfe2ec86c342cfc1e096022
Karla9191 <marietha2014@gmail.com>
2021-04-01 02:58:38 +0000

    kmkm

--- exercises/19-Looping-With-FizzBuzz/app.py
+++ exercises/19-Looping-With-FizzBuzz/app.py
@@ -1,8 +1,12 @@
 def fizz_buzz():
     # your code here
 	for i in range(1,101):
-        
-		print(i)
+        if i% 3 == 0 and i % 5 != 0:
+            print(f"{i} es múltiplo de dos")
+        elif i % 5 == 0:
+            print(f"{i} es múltiplo de cuatro y de dos")
+        else:
+            print(f"{i} no es múltiplo de dos")
 	return i
 
 fizz_buzz()
